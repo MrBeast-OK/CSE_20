c3f313851a7bd98497c7e356039c246a4135efb6
MrBeast-OK <krishnabansal0324@gmail.com>
2025-12-26 09:58:30 +0530

    Add files via upload

---
+++ Array_Q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10] = {};
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i = 0; i < 10; i++)
+    {   
+        cout << "Element at index " << i << ": " << arr[i] << endl;
+    }
+    return 0;
+}
