fd1d90b44547a9a0776e891addede14870a83f7c
MrBeast-OK <krishnabansal0324@gmail.com>
2025-12-26 09:57:22 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+#Ques. 
+
+a = float(input("first number :" ))
+b = float(input("second number :"))
+sum = a+b
+print ("sum :", sum)
+
+difference = a-b
+print("difference :", difference)
+
+product = a*b
+print("product :", product)
+
+divide = a/b
+print ("divide : ", divide)
+
+remainder = a%b
+print("remainder : ", remainder)
+
+power = a**b
+print("power :", power)
