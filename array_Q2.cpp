3fdc94afc81d3dd5757867c71e302cc69de1f1ee
MrBeast-OK <krishnabansal0324@gmail.com>
2025-12-26 09:59:21 +0530

    Add files via upload

---
+++ array_Q2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5], sum=0;
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i = 0; i < 5; i++)
+    {   
+        sum = sum + arr[i];
+    }
+    cout<<"sum: "<<sum<<endl;
+    return 0;
+}
