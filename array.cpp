2ded0db44f93289fdacc777efec3840f5a3da8cf
MrBeast-OK <krishnabansal0324@gmail.com>
2025-12-26 09:58:12 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i = 0; i < 5; i++)
+    {   
+        cout << "Element at index " << i << ": " << arr[i] << endl;
+    }
+    return 0;
+}
