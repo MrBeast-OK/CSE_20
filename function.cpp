f84aa06a3c6b7035b39f0f4c5fb3a06571678928
MrBeast-OK <krishnabansal0324@gmail.com>
2025-12-26 11:08:33 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int, int);
+int main()
+{
+ int a,b,x;
+ cin>>a>>b;
+ x =power(a,b);
+ cout<<x;
+}
+int power(int x, int y)
+{
+    int r=1;
+    for(int c=1; c<=y; c++)
+    {
+        r = r*x;
+    }
+    return(r);
+}
